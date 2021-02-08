2c4b4b5dcf190f82977c03dde727544b2b1ff004
Ahnaf2727 <captain.ahnaf27@gmail.com>
2021-02-08 10:10:47 +0700

    Initial Commit

---
+++ NewsUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsUi
+//
+//  Created by MacBook on 08/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
